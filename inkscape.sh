17c65b70cda5e20f8176f8e2cddf6f293afa0f56
Joakim Lundborg <joakim.lundborg@gmail.com>
2012-10-20 21:21:57 +0200

    Add script for creating icon in various sizes

---
+++ inkscape.sh
@@ -0,0 +1,8 @@
+
+
+inkscape payday.svg -w 512 -h 512 -e res/drawable/applogo_payday.png
+inkscape payday.svg -w 36 -h 36 -e res/drawable-ldpi/ic_launcher.png
+inkscape payday.svg -w 48 -h 48 -e res/drawable-mdpi/ic_launcher.png 
+inkscape payday.svg -w 72 -h 72 -e res/drawable-hdpi/ic_launcher.png
+inkscape payday.svg -w 96 -h 96 -e res/drawable-xhdpi/ic_launcher.png
+
