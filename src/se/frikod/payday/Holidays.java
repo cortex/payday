76116d24f56cd41f1712b1141be776c550341d4b
Joakim Lundborg <lundborg@wrapp.com>
2013-01-20 21:06:08 +0100

    Remove unnecessary initialization

--- src/se/frikod/payday/Holidays.java
+++ src/se/frikod/payday/Holidays.java
@@ -25,7 +25,7 @@ public class Holidays {
 	public static String convertStreamToString(InputStream is) throws Exception {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 		StringBuilder sb = new StringBuilder();
-		String line = null;
+		String line;
 
 		while ((line = reader.readLine()) != null) {
 			sb.append(line);
