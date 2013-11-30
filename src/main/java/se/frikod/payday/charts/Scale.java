4627aa6566f7f6cedb344040d4c3df28ef65b8e5
Joakim Lundborg <joakim.lundborg@gmail.com>
2013-11-30 23:55:05 +0100

    Standard gradle layout

---
+++ src/main/java/se/frikod/payday/charts/Scale.java
@@ -0,0 +1,22 @@
+package se.frikod.payday.charts;
+
+/**
+ * Created by joakim on 11/28/13.
+ */
+
+public class Scale{
+    private double k;
+    private double m;
+
+    public void recalc(Number inMin, Number inMax, double outMin, double outMax){
+        k = (outMax - outMin) / (inMax.doubleValue() - inMin.doubleValue());
+        m = outMin - k*inMin.doubleValue();
+        //Log.i("Payday.Scale", String.format("inMin:%f inMax:%f", inMin.doubleValue(), inMax.doubleValue()));
+        //Log.i("Payday.Scale", String.format("outMin:%f outMax:%f", outMin, outMax));
+        //Log.i("Payday.Scale", String.format("k:%f m:%f", k, m));
+    }
+
+    public double transform(Number value){
+        return value.doubleValue()* k + m;
+    }
+}
---
+++ tests/java/se/frikod/payday/charts/ScaleTest.java
@@ -0,0 +1,30 @@
+package se.frikod.payday.charts;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by joakim on 11/29/13.
+ */
+public class ScaleTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testRecalc() throws Exception {
+
+    }
+
+    @Test
+    public void testTransform() throws Exception {
+
+    }
+}
